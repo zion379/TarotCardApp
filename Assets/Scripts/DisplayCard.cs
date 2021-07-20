6d96e06220a2a682c127b83a899f1079afded3c0
zion379 <zspynet22@icloud.com>
2021-07-20 12:49:13 -0700

    Created Project
    Added editor features to create cards and example scene to preview cards.

---
+++ Assets/Scripts/DisplayCard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DisplayCard : MonoBehaviour
+{
+    public TextMeshProUGUI cardNameText;
+    public TextMeshProUGUI romanNumeralText;
+    public Image backgroundImageRefrence;
+    public Image cardImageRefrence;
+
+    public MajorArcana majorArcanaCardData;
+    public MinorArcana minorArcanaCardData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (majorArcanaCardData == null)
+        {
+            cardNameText.text = minorArcanaCardData.cardName;
+            romanNumeralText.text = minorArcanaCardData.romanNumeral;
+            backgroundImageRefrence.sprite = minorArcanaCardData.cardBackGround;
+            cardImageRefrence.sprite = minorArcanaCardData.cardImage;
+        }
+        else if (minorArcanaCardData == null) 
+        {
+            cardNameText.text = majorArcanaCardData.cardName;
+            romanNumeralText.text = majorArcanaCardData.romanNumeral;
+            backgroundImageRefrence.sprite = majorArcanaCardData.cardBackGround;
+            cardImageRefrence.sprite = majorArcanaCardData.cardImage;
+        }
+    }
+
+}
---
+++ Assets/Scripts/MajorArcana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewMarjorArcanaCard",menuName ="Cards/MajorArcana")]
+public class MajorArcana : ScriptableObject
+{
+    public string cardName;
+    public string romanNumeral;
+    public string backStory;
+
+    public enum cardType {cups, pentacles, swords, wands, none};
+    public cardType typeOfCard = cardType.none;
+
+    public Sprite cardImage;
+    public Sprite cardBackGround;
+}
